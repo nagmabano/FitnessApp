b1e4557d30f64555c6ee1a611a84e55d0f982202
nagma <bano.nagma.1@gmail.com>
2020-03-28 20:03:36 +0530

    auth file added for signup and login

--- fitness-app/src/app/app.module.ts
+++ fitness-app/src/app/app.module.ts
@@ -5,10 +5,14 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
