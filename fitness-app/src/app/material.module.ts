c556478b95b4b77456c8b22333d636f6d2d22e44
nagma <bano.nagma.1@gmail.com>
2020-06-27 13:33:17 +0530

    spinner added to the training section

--- fitness-app/src/app/material.module.ts
+++ fitness-app/src/app/material.module.ts
@@ -12,6 +12,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const MaterialModulesArray = [
     MatButtonModule, 
@@ -26,7 +27,8 @@ const MaterialModulesArray = [
     MatListModule,
     MatTabsModule,
     MatCardModule,
-    MatSelectModule
+    MatSelectModule,
+    MatProgressSpinnerModule
 ];
 
 @NgModule({
