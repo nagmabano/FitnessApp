475d5f67bcd64b7e58b9c56591bdba77b7959011
nagma <bano.nagma.1@gmail.com>
2020-06-27 19:50:15 +0530

    added exit and continue option on current training

--- fitness-app/src/app/training/current-training/current-training.component.ts
+++ fitness-app/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training.component';
 
@@ -9,18 +9,24 @@ import { StopTrainingComponent } from './stop-training.component';
 })
 export class CurrentTrainingComponent implements OnInit {
 
+  @Output() trainingExit = new EventEmitter<void>();
+
   progress = 0;
   timer: any;
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.startOrResumeTimer()
+  }
+
+  startOrResumeTimer() {
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
       if (this.progress === 100) {
         clearInterval(this.timer);
       }
-    },1000)
+    },1000);
   }
 
   onStop() {
@@ -30,7 +36,11 @@ export class CurrentTrainingComponent implements OnInit {
     }});
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);      
+      if(result){
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }     
     });
   }
 
