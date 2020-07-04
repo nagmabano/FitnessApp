f1de497f86d4f85e2cef7426bc7bb56ba00464e0
nagma <bano.nagma.1@gmail.com>
2020-07-04 20:30:23 +0530

    Handling the active training via service

--- fitness-app/src/app/training/training.service.ts
+++ fitness-app/src/app/training/training.service.ts
@@ -24,4 +24,8 @@ export class TrainingService {
         this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
         this.exerciseChanged.next({...this.runningExercise});
     }
+
+    getRunningExercise() {
+        return {...this.runningExercise};
+    }
 }
