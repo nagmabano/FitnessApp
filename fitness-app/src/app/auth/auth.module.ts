be9a244f7a78488fc23b94dd6dfc86266c7bfbe9
nagma <bano.nagma.1@gmail.com>
2020-08-17 18:49:45 +0530

    splitting up Routes

--- fitness-app/src/app/auth/auth.module.ts
+++ fitness-app/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { SharedModule } from '../shared/shared.module';
     imports: [
         ReactiveFormsModule,
         AngularFireAuthModule,
+        AuthRoutingModule,
         SharedModule
     ]
 })
